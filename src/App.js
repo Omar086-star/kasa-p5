bb85999581949266209bd7bd3d5b4f6a635345fe
omar mulla <mullaomar086@gmail.com>
2024-07-10 21:27:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Navbar from "./components/Navbar.js"; // Assurez-vous que le nom du fichier est correct
+import Banner from "./components/Banner.js";
+import "./App.css";
+import Article from "./components/Article.js"
+// import Footer from "./components/Footer.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+         
+            <Banner />
+            <Article/>
+ 
+            
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider , Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const HeaderFooterLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HeaderFooterLayout />,
+    errorElement: <h1>404 not found</h1>,
+    children: [
+      {
+        path: '/',
+        element: <App />
+      },
+      {
+        path: '/Flat',
+        element: <h1>Propos</h1>
+      },
+      {
+        path: '/About',
+        element: <h1>Propos</h1>
+      }
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
