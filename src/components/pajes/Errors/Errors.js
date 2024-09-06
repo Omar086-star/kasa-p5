8b66ee1706c9fcfe54772fe075940eeb2a9d9d9c
omar mulla <mullaomar086@gmail.com>
2024-09-06 21:45:33 +0200

    test le projet avant le pusher

--- src/components/pajes/Errors/Errors.js
+++ src/components/pajes/Errors/Errors.js
@@ -5,7 +5,7 @@ function Errors(){
 
     return <div className='pajeError'>
     <h1 className='error404'>404</h1> 
-    <p className='paragraphError'>Oups! La page que vous demandez n'existe pas.</p>
+    <p className='paragraphError'>Oups! La page que <br className="brLigne"></br> vous demandez n'existe pas.</p>
     <Link to={"/About"} > <p className="retornPage">Retourner sur la page d’accueil</p></Link>
     </div>
 
